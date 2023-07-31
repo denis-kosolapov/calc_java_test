ce71d669a5d9c4691b6783e7e77816c2caad4b3c
artificial intelligence software engineer <scisoftdev@gmail.com>
2023-07-31 15:17:23 +0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -36,6 +36,15 @@ public class Main {
         if (isFirstRoman) {
             a = fromRomanNumeral(firstOperand);
             b = fromRomanNumeral(secondOperand);
+            if (a < 0 || a > 10) {
+                System.out.println("Римское число больше 10 или меньше 0");
+                throw new IllegalArgumentException("Арабское число больше 10 или меньше 0");
+            }
+            if (b < 0 || b > 10) {
+                System.out.println("Римское число больше 10 или меньше 0");
+                throw new IllegalArgumentException("Арабское число больше 10 или меньше 0");
+            }
+
         } else {
             a = Integer.parseInt(firstOperand);
             b = Integer.parseInt(secondOperand);
